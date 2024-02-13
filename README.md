732c8c3a5cb30ab844046238007bad63f58e1c29
frukcharkrit <chakrit.ph.66@ubu.ac.th>
2024-02-13 16:36:28 +0700

    force push

--- README.md
+++ README.md
@@ -6,3 +6,25 @@
 สำหรับรายวิชา [OOP](https://frukcharkrit.github.io/)
 
 ลงชื่อ - ชาคริต พิมพ์สระเกษ
+
+# เริ่มต้นพัฒนา streamlit
+
+1. clone จาก github
+
+git clone https://github.com/frukcharkrit/frukcharkrit.github.io.git
+
+2. deactivate
+
+conda deactivate
+
+3. สร้างสภาพแวดล้อมใหม่
+
+python -m venv venv
+
+4. activate สภาพแวดล้อมใหม่
+
+venv/scripts/activate
+
+5. ติดตั้ง streamlit
+
+pip install streamlit
---
+++ streamlit/app02.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('OOP in ML')
+st.sidebar.text('Sidebar')
+st.sidebar.button('GO >')
+st.sidebar.button('WENT >')
+st.sidebar.button('GONE >')
+
+st.title('OOP in ML')
+st.image('https://cdn2.unrealengine.com/egs-starwarsbattlefrontiicelebrationedition-dice-g1a-01-1920x1080-87971829e831.jpg')
+st.text_input("What's your name?")
+# git config --global user.name"fruckcharkrit"
+# git config --global user.email"chakrit.ph.66@ubu.ac.th" 
